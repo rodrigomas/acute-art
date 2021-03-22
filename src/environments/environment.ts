edf99ec0821bab19890ce85886ceee32d222816b
Pascal Brun <brunpasc@gmail.com>
2021-03-22 12:24:27 +0100

    feat(app): updates interim contracts & metadata

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,14 +4,14 @@
 
 export const environment = {
   production: false,
-  acuteartContract: 'KT1XUNEnrWeE8PCZiVSZ2yD3npQSsEBSKzVA',
-  acuteartAuctionContract: 'KT1A8nzH719JHKxnzBRFgzQZ2pmkat24JPwc',
+  acuteartContract: 'KT1DySdxvUjKqL7Hz44zMqKaXTUsdfnnVGxo',
+  acuteartAuctionContract: 'KT1BWSNSyX37VHXJwYYdCcrE7vreW5jrHgxN',
   rpcUrl: 'https://testnet-tezos.giganode.io',
   appName: 'Acute Art',
   artworkBigmapUrl:
-    'https://api.better-call.dev/v1/bigmap/edo2net/35438/keys?q=&offset=0&size=10',
+    'https://api.better-call.dev/v1/bigmap/edo2net/38379/keys?q=&offset=0&size=10',
   auctionBigmapUrl:
-    'https://api.better-call.dev/v1/bigmap/edo2net/35435/keys?q=&offset=0&size=10',
+    'https://api.better-call.dev/v1/bigmap/edo2net/38376/keys?q=&offset=0&size=10',
   indexerUrl: 'https://tzcolors-indexer.prod.gke.papers.tech/api/v1/',
   backendUrl: 'https://acute-arts-marketplace-backend.dev.gke.papers.tech',
   backendApiKey: 'api:XmPS4fIKXm0N',
